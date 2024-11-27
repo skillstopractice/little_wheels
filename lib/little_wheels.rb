ba39a9a6a43737afa2eb44269007ba6e338cdbb4
Gregory Brown <gregory@practicingdeveloper.com>
2024-11-27 08:44:11 -0500

    Fix template rendering. Set some defaults

--- lib/little_wheels.rb
+++ lib/little_wheels.rb
@@ -1,17 +1,25 @@
 module LittleWheels
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 
   class Component
     def t(template, **locals)
-      renderer.render(:inline => template, locals:)
+      renderer.render(inline: template, locals:)
     end
 
     def t!(template, **locals)
-      renderer.render(template, locals:)
+      renderer.render(template, locals:, layout: nil)
+    end
+
+    def to_s
+      t!("shared/#{name}", c: self)
     end
 
     def renderer
       ApplicationController
+    end
+
+    def name
+      self.class.name.underscore
     end
   end
 end
